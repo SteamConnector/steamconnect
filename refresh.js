a96b0d0af75a337d391b4d229b18da57e3a85302
dani <dangreene0@gmail.com>
2024-09-28 20:48:10 +0000

    test: switch to just window.location

--- refresh.js
+++ refresh.js
@@ -1,8 +1,8 @@
 // ad-free scripting!
-var link = window.location.href
+var link = window.location.href;
 var server = link.split("?")[1];
 if (server != undefined && !link.endsWith("?")) {
-    window.location = "steam://connect/" + server; 
-    var refresh = '<meta http-equiv="refresh" content="0;url=' + window.location + '">';
-    document.write(refresh);
+    window.location = "steam://connect/" + server;
+    // var refresh = '<meta http-equiv="refresh" content="0;url=' + connect + '">';
+    // document.write(refresh);
 } 
