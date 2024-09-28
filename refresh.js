2c6c22b7a5c755df162ed18dc4f33e3250b9b5a5
dani <dangreene0@gmail.com>
2024-09-28 20:49:49 +0000

    test: using just document.write

--- refresh.js
+++ refresh.js
@@ -2,7 +2,7 @@
 var link = window.location.href;
 var server = link.split("?")[1];
 if (server != undefined && !link.endsWith("?")) {
-    window.location = "steam://connect/" + server;
-    // var refresh = '<meta http-equiv="refresh" content="0;url=' + connect + '">';
-    // document.write(refresh);
+    //window.location = "steam://connect/" + server;
+    var refresh = '<meta http-equiv="refresh" content="0;url=' + connect + '">';
+    document.write(refresh);
 } 
