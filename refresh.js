38235602a42684d52349d5a674b9a70cf2cd364f
dani <dangreene0@gmail.com>
2024-09-28 21:19:22 +0000

    refactor: simplify script

--- refresh.js
+++ refresh.js
@@ -1,8 +1,4 @@
-// ad-free scripting!
-var link = window.location.href;
-var server = link.split("?")[1];
-if (server != undefined && !link.endsWith("?")) {
-    //window.location = "steam://connect/" + server;
-    var refresh = '<meta http-equiv="refresh" content="0;url=' + connect + '">';
-    document.write(refresh);
+let server = location.href.split("?")[1];
+if (server != undefined && !location.href.endsWith("?")) {
+    location = "steam://connect/" + server;
 } 
